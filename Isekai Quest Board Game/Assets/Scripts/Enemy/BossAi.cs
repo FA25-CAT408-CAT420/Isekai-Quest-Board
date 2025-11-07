cac0d39df2f11e782e24a7c326773affcea551e1
DanielTheNerd35 <vazquezd8@montclair.edu>
2025-11-07 18:58:44 -0500

    Worked on Boss

---
+++ Isekai Quest Board Game/Assets/Scripts/Enemy/BossAi.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAi : EnemyCore
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpInstances();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
