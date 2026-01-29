a639cd42df845b0519143f46f2257ddb3bd26496
threeboysinsideatrenchcoat <157040871+threeboysinsideatrenchcoat@users.noreply.github.com>
2026-01-29 13:58:53 -0500

    01/29
    -Added Music
    -Made Audio Manager for SFX

---
+++ Isekai Quest Board Game/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+   [SerializeField] AudioSource musicSource;
+   [SerializeField] AudioSource SFXSource;
+
+   public AudioClip sword1; 
+   public AudioClip sword2;
+   public AudioClip potion;
+   public AudioClip levelUp;
+   public AudioClip selectionSFX; 
+   public AudioClip positiveSFX;
+   public AudioClip slimeHurt;
+   public AudioClip playerHurt;
+   public AudioClip nPCSpeak;
+   public AudioClip videoGameTXT; 
+}
+ public void PlaySFX(AudioClip clip)
+ {
+    SFXSource.PlayOneShot(clip);
+ }
