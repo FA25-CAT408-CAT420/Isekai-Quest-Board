4904df675d459f46914c3b678dd3b4d6bb948b36
DanielTheNerd35 <vazquezd8@montclair.edu>
2026-03-03 14:17:37 -0500

    Added spike behavior

---
+++ Isekai Quest Board Game/Assets/Scripts/World/SpikeBehavior.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeBehavior : MonoBehaviour
+{
+    /* Note for self. Make a list of all the spikes then make a switch statement where I assign each spike's numercial value to a Letter value. Then have each one play their animation.
+    */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
